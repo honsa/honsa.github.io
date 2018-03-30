1f9ef86ceb341c5678a1c4ae05b9c61e91b64865
honsa <straka@8038.ch>
2018-03-30 17:18:54 +0200

    function

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
-document.querySelector('header').onmousemove = function (e) {
+let mouseTarget = function(selector){
+    document.querySelector(selector).onmousemove = function (e) {
 
-    var x = e.pageX - e.target.offsetLeft;
-    var y = e.pageY - e.target.offsetTop;
+        var x = e.pageX - e.target.offsetLeft;
+        var y = e.pageY - e.target.offsetTop;
 
-    e.target.style.setProperty('--x', x + 'px');
-    e.target.style.setProperty('--y', y + 'px');
-};
+        e.target.style.setProperty('--x', x + 'px');
+        e.target.style.setProperty('--y', y + 'px');
+    };
+}
 
-document.querySelector('.content').onmousemove = function (e) {
-
-    var x = e.pageX - e.target.offsetLeft;
-    var y = e.pageY - e.target.offsetTop;
-
-    e.target.style.setProperty('--x', x + 'px');
-    e.target.style.setProperty('--y', y + 'px');
-};
+mouseTarget('header');
+mouseTarget('.content');
