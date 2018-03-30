bbaaa67444ab31c109bb11001d6fbb32b99914db
honsa <straka@8038.ch>
2018-03-30 16:39:35 +0200

    content hover js

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ document.querySelector('.fancy').onmousemove = function (e) {
     e.target.style.setProperty('--x', x + 'px');
     e.target.style.setProperty('--y', y + 'px');
 };
+
+document.querySelector('.content').onmousemove = function (e) {
+
+    var x = e.pageX - e.target.offsetLeft;
+    var y = e.pageY - e.target.offsetTop;
+
+    e.target.style.setProperty('--x', x + 'px');
+    e.target.style.setProperty('--y', y + 'px');
+};
