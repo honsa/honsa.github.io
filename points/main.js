3ac65264fa8d6a8ab2f3c14c8116a38b57aa7d3f
honsa.ch <straka@8038.ch>
2024-02-16 19:09:05 +0100

    Points init

---
+++ points/main.js
@@ -0,0 +1,27 @@
+function createButton(name) {
+    let button = document.createElement("button");
+    button.innerHTML = name;
+    button.onclick = (e) => {
+
+        if(e.target.classList.contains("selected")) {
+            e.target.classList.remove("selected");
+        } else {
+            e.target.classList.add("selected");
+        }
+    };
+
+    document.body.appendChild(button);
+}
+
+function fibonacciNumbers() {
+    let fibNumbers = [0, 1];
+    let nextNumber = fibNumbers[fibNumbers.length - 1] + fibNumbers[fibNumbers.length - 2];
+
+    while (nextNumber <= 21) {
+        fibNumbers.push(nextNumber);
+        createButton(nextNumber);
+        nextNumber = fibNumbers[fibNumbers.length - 1] + fibNumbers[fibNumbers.length - 2];
+    }
+}
+
+fibonacciNumbers();
