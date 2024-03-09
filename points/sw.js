6c25d18729899c220995b20d8cd7f0234302fb3b
honsa.ch <straka@8038.ch>
2024-03-09 07:45:58 +0100

    Serviceworker update, cache all files, delete old caches

--- points/sw.js
+++ points/sw.js
@@ -1,15 +1,45 @@
+const staticCacheName = 'points-pwa-0.0.1';
+const filesToCache = [
+    '/',
+    '/index.html',
+    '/main.css',
+    '/main.js'
+];
+
+/**
+ * Add the files from the cache
+ */
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('sw-cache').then((cache) =>{
-            return cache.add('index.html');
-        })
+        caches.open(staticCacheName)
+            .then(cache => cache.addAll(filesToCache))
     );
 });
 
+/**
+ * Serve the files from the cache
+ */
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) =>{
-            return response || fetch(event.request);
-        })
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+/**
+ * Delete outdated cache versions
+ */
+self.addEventListener('activate', event => {
+    const currentCacheNames = Object.keys(caches);
+    event.waitUntil(
+        Promise.all(
+            currentCacheNames.map(cacheName => {
+                if (cacheName !== staticCacheName) {
+                    return caches.delete(cacheName);
+                }
+            })
+        )
     );
 });
